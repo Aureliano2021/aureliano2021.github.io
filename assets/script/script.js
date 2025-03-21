6f02601c53b44a58cdd14315bd9b9dff460d7a15
Aureliano2021 <aureliano.oliveira2011@gmail.com>
2025-03-21 13:13:39 -0300

    Inserido a paleta de cores, o nav, estilizando as classes e dando funções aos botões do nav, além de adicionar links nas certificações.

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,3 +8,27 @@ function caixaAlta(value){
 caixaAlta('header h1')
 
 caixaAlta('main h2')
+
+document.querySelector('.btn_objetivo').addEventListener('click', () => {
+    document.querySelector('.objetivo').scrollIntoView({behavior: 'smooth'})
+})
+
+document.querySelector('.btn_profissional').addEventListener('click', () => {
+    document.querySelector('.profissional').scrollIntoView({behavior: 'smooth'})
+})
+
+document.querySelector('.btn_academico').addEventListener('click', () => {
+    document.querySelector('.academico').scrollIntoView({behavior: 'smooth'})
+})
+
+document.querySelector('.btn_certificacoes').addEventListener('click', () => {
+    document.querySelector('.certificacoes').scrollIntoView({behavior: 'smooth'})
+})
+
+document.querySelector('.btn_softskills').addEventListener('click', () => {
+    document.querySelector('.softskills').scrollIntoView({behavior: 'smooth'})
+})
+
+document.querySelector('.logo').addEventListener('click', () => {
+    document.querySelector('header').scrollIntoView({behavior: 'smooth'})
+})
