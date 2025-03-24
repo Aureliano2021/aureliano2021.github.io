3b0504464395b7a8792c151469dc7bd9067f36bf
Aureliano2021 <aureliano.oliveira2011@gmail.com>
2025-03-24 14:01:30 -0300

    Melhorando responsividade

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,78 @@
 let apertou = true
+let recarregado = true;
 
 function caixaAlta(value){
     document.querySelectorAll(value).forEach(element => {
         let caixaAltagroup = element.textContent
         element.textContent = caixaAltagroup.toUpperCase()
     });
+}
+
+function newlayout(){
+    if(window.innerWidth < 750){
+        document.querySelector('.objetivo').innerHTML = 
+        `
+            <h2>Objetivo</h2>
+            <div>Em busca de oportunidade como Desenvolvedor de Softwares ou na área de Tecnologia da Informação.</div>
+        `
+    
+        document.querySelector('.profissional').innerHTML = 
+        `
+            <h2>Histórico Profissional</h2>
+                    <div style="margin-bottom: 25px">
+                        <div>08/2022 - 07/2023</div>
+                        <div>Coordenador Censitário de Subárea<br>
+                            IBGE – São João, PE</div>
+                    </div>
+                    <div style="margin-bottom: 25px">
+                        <div>02/2014 - 01/2015</div>
+                        <div>Aprendiz de Eletricista Industrial<br>
+                            CONEST/RNEST – Cabo de Santo Agostinho, PE</div>
+                    </div>
+        `
+        document.querySelector('.academico').innerHTML = 
+        `
+            <h2>Formação Acadêmica</h2>
+                    <div style="margin-bottom: 25px">
+                        
+                        <div><strong>Graduação Tecnológica em Análise e Desenvolvimento de Sistemas</strong><br>
+                        Centro Universitário Estácio de Ribeirão Preto – Ribeirão Preto, SP</div>
+                        <div>Concluído em Abril/2024</div>
+                    </div>
+                    <div style="margin-bottom: 30px">
+                        
+                        <div><strong>Ensino Médio Completo</strong><br>Escola Estadual Madre Iva Bezerra de Araújo – Cabo de Santo Agostinho, PE</div>
+                        <div>Concluído em <br>Dezembro/2010</div>
+                    </div>
+        `
+    
+        document.querySelector('.certificacoes').innerHTML = 
+        `
+            <h2>Certificações</h2>
+                    <ul>
+                        <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_8456645.jpg" target="_blank">HTML5 e CSS3 | B7WEB<br>92 horas | 2025</a></li>
+                        <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3117813.jpg" target="_blank">JavaScript | B7WEB<br>66 horas | 2025</a></li>
+                        <li><a href="https://www.dio.me/certificate/F577873D/share" target="_blank">Lógica de Programação Essencial | DIO<br>4 horas | 2021</a></li>
+                        <li><a href="https://www.dio.me/certificate/B16DD93C/share" target="_blank">Introdução ao Git e Github | DIO<br>5 horas | 2021</a></li>
+                    </ul>
+        `
+        document.querySelector('.softskills').innerHTML = 
+        `
+            <h2>Habilidades e Competências</h2>
+                    <ul>
+                        <li>Facilidade para aprendizagem de novas tecnologias.</li>
+                        <li>Domínio de versionamento de código com Git e Github</li>
+                        <li>Conhecimento em lógica de programação.</li>
+                        <li>Capacidade para trabalhar em equipe.</li>
+                    </ul>
+        `
+        recarregado = false
+    } else{
+        if (!recarregado) {
+            recarregado = true;
+            location.reload(); 
+        }
+    }
 }
 
 document.querySelectorAll('.btn_objetivo').forEach((e) => e.addEventListener('click', () => {
@@ -56,65 +124,11 @@ document.querySelector(".btn_menuMobile").addEventListener('click',function novo
     }
 })
 
-if(screen.width < 750){
-    document.querySelector('.objetivo').innerHTML = 
-    `
-        <h2>Objetivo</h2>
-        <div>Em busca de oportunidade como Desenvolvedor de Softwares ou na área de Tecnologia da Informação.</div>
-    `
+newlayout()
 
-    document.querySelector('.profissional').innerHTML = 
-    `
-        <h2>Histórico Profissional</h2>
-                <div style="margin-bottom: 25px">
-                    <div>08/2022 - 07/2023</div>
-                    <div>Coordenador Censitário de Subárea<br>
-                        IBGE – São João, PE</div>
-                </div>
-                <div style="margin-bottom: 25px">
-                    <div>02/2014 - 01/2015</div>
-                    <div>Aprendiz de Eletricista Industrial<br>
-                        CONEST/RNEST – Cabo de Santo Agostinho, PE</div>
-                </div>
-    `
-    document.querySelector('.academico').innerHTML = 
-    `
-        <h2>Formação Acadêmica</h2>
-                <div style="margin-bottom: 25px">
-                    
-                    <div><strong>Graduação Tecnológica em Análise e Desenvolvimento de Sistemas</strong><br>
-                    Centro Universitário Estácio de Ribeirão Preto – Ribeirão Preto, SP</div>
-                    <div>Concluído em Abril/2024</div>
-                </div>
-                <div style="margin-bottom: 30px">
-                    
-                    <div><strong>Ensino Médio Completo</strong><br>Escola Estadual Madre Iva Bezerra de Araújo – Cabo de Santo Agostinho, PE</div>
-                    <div>Concluído em <br>Dezembro/2010</div>
-                </div>
-    `
-
-    document.querySelector('.certificacoes').innerHTML = 
-    `
-        <h2>Certificações</h2>
-                <ul>
-                    <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_8456645.jpg" target="_blank">HTML5 e CSS3 | B7WEB<br>92 horas | 2025</a></li>
-                    <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3117813.jpg" target="_blank">JavaScript | B7WEB<br>66 horas | 2025</a></li>
-                    <li><a href="https://www.dio.me/certificate/F577873D/share" target="_blank">Lógica de Programação Essencial | DIO<br>4 horas | 2021</a></li>
-                    <li><a href="https://www.dio.me/certificate/B16DD93C/share" target="_blank">Introdução ao Git e Github | DIO<br>5 horas | 2021</a></li>
-                </ul>
-    `
-    document.querySelector('.softskills').innerHTML = 
-    `
-        <h2>Habilidades e Competências</h2>
-                <ul>
-                    <li>Facilidade para aprendizagem de novas tecnologias.</li>
-                    <li>Domínio de versionamento de código com Git e Github</li>
-                    <li>Conhecimento em lógica de programação.</li>
-                    <li>Capacidade para trabalhar em equipe.</li>
-                </ul>
-    `
-}
+window.addEventListener('resize', newlayout)
 
 caixaAlta('header h1')
 
 caixaAlta('main h2')
+
