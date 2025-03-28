19140ae0392b52d8d61ea5a933e2a848b30fe819
Aureliano2021 <aureliano.oliveira2011@gmail.com>
2025-03-28 15:50:15 -0300

    Implementando TypeScript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,126 +1,79 @@
-let apertou = true
-let recarregado = true;
-
-function newlayout(){
-    if(window.innerWidth < 750){
-        document.querySelector('.objetivo').innerHTML = 
-        `
-            <h2>Objetivo</h2>
-            <div>Em busca de oportunidade como Desenvolvedor de Softwares ou na área de Tecnologia da Informação.</div>
-        `
-    
-        document.querySelector('.profissional').innerHTML = 
-        `
-            <h2>Histórico Profissional</h2>
-                    <div style="margin-bottom: 25px">
-                        <div>08/2022 - 07/2023</div>
-                        <div>Coordenador Censitário de Subárea<br>
-                            IBGE – São João, PE</div>
-                    </div>
-                    <div style="margin-bottom: 25px">
-                        <div>02/2014 - 01/2015</div>
-                        <div>Aprendiz de Eletricista Industrial<br>
-                            CONEST/RNEST – Cabo de Santo Agostinho, PE</div>
-                    </div>
-        `
-        document.querySelector('.academico').innerHTML = 
-        `
-            <h2>Formação Acadêmica</h2>
-                    <div style="margin-bottom: 25px">
-                        
-                        <div><strong>Graduação Tecnológica em Análise e Desenvolvimento de Sistemas</strong><br>
-                        Centro Universitário Estácio de Ribeirão Preto – Ribeirão Preto, SP</div>
-                        <div>Concluído em Abril/2024</div>
-                    </div>
-                    <div style="margin-bottom: 30px">
-                        
-                        <div><strong>Ensino Médio Completo</strong><br>Escola Estadual Madre Iva Bezerra de Araújo – Cabo de Santo Agostinho, PE</div>
-                        <div>Concluído em <br>Dezembro/2010</div>
-                    </div>
-        `
-    
-        document.querySelector('.certificacoes').innerHTML = 
-        `
-            <h2>Certificações</h2>
-                    <ul>
-                        <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_8456645.jpg" target="_blank">HTML5 e CSS3 | B7WEB<br>92 horas | 2025</a></li>
-                        <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3117813.jpg" target="_blank">JavaScript | B7WEB<br>66 horas | 2025</a></li>
-                        <li><a href="https://www.dio.me/certificate/F577873D/share" target="_blank">Lógica de Programação Essencial | DIO<br>4 horas | 2021</a></li>
-                        <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3017556.jpg" target="_blank">Git/Github | B7WEB<br>20 horas | 2025</a></li>
-                    </ul>
-        `
-        document.querySelector('.softskills').innerHTML = 
-        `
-            <h2>Habilidades e Competências</h2>
-                    <ul>
-                        <li>Facilidade para aprendizagem de novas tecnologias.</li>
-                        <li>Domínio de versionamento de código com Git e Github</li>
-                        <li>Conhecimento em lógica de programação.</li>
-                        <li>Capacidade para trabalhar em equipe.</li>
-                    </ul>
-        `
-        recarregado = false
-    } else{
-        if (!recarregado) {
-            recarregado = true;
-            location.reload(); 
+var apertouMenu = true;
+var recarregadoElement = true;
+var c = function (selector) { return document.querySelector(selector); };
+var objetivoElement = c('.objetivo');
+var profissionalElement = c('.profissional');
+var academicoElement = c('.academico');
+var certificacoesElement = c('.certificacoes');
+var softskillsElement = c('.softskills');
+var btnMenuMobileElement = c(".btn_menuMobile");
+var asideMenuMobile = c("aside .menuMobile");
+var logoElement = c('.logo');
+var headerElement = c('header');
+function newlayout2() {
+    if (window.innerWidth < 750) {
+        objetivoElement.innerHTML =
+            "\n            <h2>Objetivo</h2>\n            <div>Em busca de oportunidade como Desenvolvedor de Softwares ou na \u00E1rea de Tecnologia da Informa\u00E7\u00E3o.</div>\n        ";
+        profissionalElement.innerHTML =
+            "\n            <h2>Hist\u00F3rico Profissional</h2>\n            <div style=\"margin-bottom: 25px\">\n                <div>08/2022 - 07/2023</div>\n                <div>Coordenador Censit\u00E1rio de Sub\u00E1rea<br>IBGE \u2013 S\u00E3o Jo\u00E3o, PE</div>\n            </div>\n            <div style=\"margin-bottom: 25px\">\n                <div>02/2014 - 01/2015</div>\n                <div>Aprendiz de Eletricista Industrial<br>CONEST/RNEST \u2013 Cabo de Santo Agostinho, PE</div>\n            </div>\n        ";
+        academicoElement.innerHTML =
+            "\n            <h2>Forma\u00E7\u00E3o Acad\u00EAmica</h2>\n            <div style=\"margin-bottom: 25px\">\n                <div><strong>Gradua\u00E7\u00E3o Tecnol\u00F3gica em An\u00E1lise e Desenvolvimento de Sistemas</strong><br>Centro Universit\u00E1rio Est\u00E1cio de Ribeir\u00E3o Preto \u2013 Ribeir\u00E3o Preto, SP</div>\n                <div>Conclu\u00EDdo em Abril/2024</div>\n            </div>\n            <div style=\"margin-bottom: 30px\">\n                            \n                <div><strong>Ensino M\u00E9dio Completo</strong><br>Escola Estadual Madre Iva Bezerra de Ara\u00FAjo \u2013 Cabo de Santo Agostinho, PE</div>\n                <div>Conclu\u00EDdo em <br>Dezembro/2010</div>\n            </div>\n        ";
+        certificacoesElement.innerHTML =
+            "\n            <h2>Certifica\u00E7\u00F5es</h2>\n            <ul>\n                <li><a href=\"https://alunos.b7web.com.br/media/certificates/certificado_8456645.jpg\" target=\"_blank\">HTML5 e CSS3 | B7WEB<br>92 horas | 2025</a></li>\n                <li><a href=\"https://alunos.b7web.com.br/media/certificates/certificado_3117813.jpg\" target=\"_blank\">JavaScript | B7WEB<br>66 horas | 2025</a></li>\n                <li><a href=\"https://www.dio.me/certificate/F577873D/share\" target=\"_blank\">L\u00F3gica de Programa\u00E7\u00E3o Essencial | DIO<br>4 horas | 2021</a></li>\n                <li><a href=\"https://alunos.b7web.com.br/media/certificates/certificado_3017556.jpg\" target=\"_blank\">Git/Github | B7WEB<br>20 horas | 2025</a></li>\n            </ul>\n        ";
+        softskillsElement.innerHTML =
+            "\n            <h2>Habilidades e Compet\u00EAncias</h2>\n            <ul>\n                <li>Facilidade para aprendizagem de novas tecnologias.</li>\n                <li>Dom\u00EDnio de versionamento de c\u00F3digo com Git e Github</li>\n                <li>Conhecimento em l\u00F3gica de programa\u00E7\u00E3o.</li>\n                <li>Capacidade para trabalhar em equipe.</li>\n            </ul>\n        ";
+        recarregadoElement = false;
+    }
+    else {
+        if (!recarregadoElement) {
+            recarregadoElement = true;
+            location.reload();
         }
     }
 }
-
-function whatsApp(){
-    window.open('https://wa.me/5587991081827','_blank')
+function whatsApp2() {
+    window.open('https://wa.me/5587991081827', '_blank');
 }
-
-document.querySelectorAll('.btn_objetivo').forEach((e) => e.addEventListener('click', () => {
-    document.querySelector('.objetivo').scrollIntoView({behavior: 'smooth'})
-    })
-)
-
-document.querySelectorAll('.btn_profissional').forEach((e) => e.addEventListener('click', () => {
-    document.querySelector('.profissional').scrollIntoView({behavior: 'smooth'})
-    })
-)
-
-document.querySelectorAll('.btn_academico').forEach((e) => e.addEventListener('click', () => {
-    document.querySelector('.academico').scrollIntoView({behavior: 'smooth'})
-    })
-)
-
-document.querySelectorAll('.btn_certificacoes').forEach((e) => e.addEventListener('click', () => {
-    document.querySelector('.certificacoes').scrollIntoView({behavior: 'smooth'})
-    })
-)
-
-document.querySelectorAll('.btn_softskills').forEach((e) => e.addEventListener('click', () => {
-    document.querySelector('.softskills').scrollIntoView({behavior: 'smooth'})
-    })
-)
-
-document.querySelector('.logo').addEventListener('click', () => {
-    document.querySelector('header').scrollIntoView({behavior: 'smooth'})
-})
-
-document.querySelector(".btn_menuMobile").addEventListener('click',function novo(e){
-    if(apertou){
-    
-        document.querySelector(".btn_menuMobile").style = 'rotate: 90deg'
-        
-        document.querySelector("aside .menuMobile").style = 'right: 0vw'
-
-        setInterval(() => apertou = false, 100)
+;
+document.querySelectorAll('.btn_objetivo').forEach(function (e) { return e.addEventListener('click', function () {
+    if (objetivoElement) {
+        objetivoElement.scrollIntoView({ behavior: 'smooth' });
     }
-
-    if(apertou === false){
-    
-        document.querySelector(".btn_menuMobile").style = 'rotate: 0deg'
-
-        document.querySelector("aside .menuMobile").style = 'right: -100%'
-        
-        setInterval(() =>apertou = true, 100)
+}); });
+document.querySelectorAll('.btn_profissional').forEach(function (e) { return e.addEventListener('click', function () {
+    if (profissionalElement) {
+        profissionalElement.scrollIntoView({ behavior: 'smooth' });
     }
-})
-
-newlayout()
-
-window.addEventListener('resize', newlayout)
+}); });
+document.querySelectorAll('.btn_academico').forEach(function (e) { return e.addEventListener('click', function () {
+    if (academicoElement) {
+        academicoElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}); });
+document.querySelectorAll('.btn_certificacoes').forEach(function (e) { return e.addEventListener('click', function () {
+    if (certificacoesElement) {
+        certificacoesElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}); });
+document.querySelectorAll('.btn_softskills').forEach(function (e) { return e.addEventListener('click', function () {
+    if (softskillsElement) {
+        softskillsElement.scrollIntoView({ behavior: 'smooth' });
+    }
+}); });
+logoElement.addEventListener('click', function () {
+    headerElement.scrollIntoView({ behavior: 'smooth' });
+});
+btnMenuMobileElement.addEventListener('click', function novo(e) {
+    if (apertouMenu) {
+        btnMenuMobileElement.style = 'rotate: 90deg';
+        asideMenuMobile.style = 'right: 0vw';
+        setInterval(function () { return apertouMenu = false; }, 100);
+    }
+    if (apertouMenu === false) {
+        btnMenuMobileElement.style = 'rotate: 0deg';
+        asideMenuMobile.style = 'right: -100%';
+        setInterval(function () { return apertouMenu = true; }, 100);
+    }
+});
+newlayout2();
+window.addEventListener('resize', newlayout2);
---
+++ assets/src/script.ts
@@ -0,0 +1,140 @@
+let apertouMenu = true;
+let recarregadoElement = true;
+const c = (selector: string) => document.querySelector(selector);
+const objetivoElement = c('.objetivo') as HTMLDivElement
+const profissionalElement = c('.profissional') as HTMLDivElement
+const academicoElement = c('.academico') as HTMLDivElement
+const certificacoesElement = c('.certificacoes') as HTMLDivElement
+const softskillsElement = c('.softskills') as HTMLDivElement
+const btnMenuMobileElement = c(".btn_menuMobile") as HTMLDivElement
+const asideMenuMobile = c("aside .menuMobile") as HTMLDivElement
+const logoElement = c('.logo') as HTMLImageElement
+const headerElement = c('header') as HTMLDivElement
+
+function newlayout2() {
+    if (window.innerWidth < 750) {
+        objetivoElement.innerHTML =
+            `
+            <h2>Objetivo</h2>
+            <div>Em busca de oportunidade como Desenvolvedor de Softwares ou na área de Tecnologia da Informação.</div>
+        `;
+        profissionalElement.innerHTML =
+        `
+            <h2>Histórico Profissional</h2>
+            <div style="margin-bottom: 25px">
+                <div>08/2022 - 07/2023</div>
+                <div>Coordenador Censitário de Subárea<br>IBGE – São João, PE</div>
+            </div>
+            <div style="margin-bottom: 25px">
+                <div>02/2014 - 01/2015</div>
+                <div>Aprendiz de Eletricista Industrial<br>CONEST/RNEST – Cabo de Santo Agostinho, PE</div>
+            </div>
+        `;
+        academicoElement.innerHTML =
+        `
+            <h2>Formação Acadêmica</h2>
+            <div style="margin-bottom: 25px">
+                <div><strong>Graduação Tecnológica em Análise e Desenvolvimento de Sistemas</strong><br>Centro Universitário Estácio de Ribeirão Preto – Ribeirão Preto, SP</div>
+                <div>Concluído em Abril/2024</div>
+            </div>
+            <div style="margin-bottom: 30px">
+                            
+                <div><strong>Ensino Médio Completo</strong><br>Escola Estadual Madre Iva Bezerra de Araújo – Cabo de Santo Agostinho, PE</div>
+                <div>Concluído em <br>Dezembro/2010</div>
+            </div>
+        `;
+        certificacoesElement.innerHTML =
+        `
+            <h2>Certificações</h2>
+            <ul>
+                <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_8456645.jpg" target="_blank">HTML5 e CSS3 | B7WEB<br>92 horas | 2025</a></li>
+                <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3117813.jpg" target="_blank">JavaScript | B7WEB<br>66 horas | 2025</a></li>
+                <li><a href="https://www.dio.me/certificate/F577873D/share" target="_blank">Lógica de Programação Essencial | DIO<br>4 horas | 2021</a></li>
+                <li><a href="https://alunos.b7web.com.br/media/certificates/certificado_3017556.jpg" target="_blank">Git/Github | B7WEB<br>20 horas | 2025</a></li>
+            </ul>
+        `;
+        softskillsElement.innerHTML =
+        `
+            <h2>Habilidades e Competências</h2>
+            <ul>
+                <li>Facilidade para aprendizagem de novas tecnologias.</li>
+                <li>Domínio de versionamento de código com Git e Github</li>
+                <li>Conhecimento em lógica de programação.</li>
+                <li>Capacidade para trabalhar em equipe.</li>
+            </ul>
+        `
+        recarregadoElement = false
+    } else {
+        if (!recarregadoElement) {
+            recarregadoElement = true;
+            location.reload();
+        }
+    }
+}
+
+function whatsApp2() {
+    window.open('https://wa.me/5587991081827','_blank');
+};
+
+document.querySelectorAll('.btn_objetivo').forEach((e) => e.addEventListener('click', () => {
+    if (objetivoElement) {
+        objetivoElement.scrollIntoView({ behavior: 'smooth' });
+    }
+})
+)
+
+document.querySelectorAll('.btn_profissional').forEach((e) => e.addEventListener('click', () => {
+    if (profissionalElement) {
+        profissionalElement.scrollIntoView({ behavior: 'smooth' });
+    }
+})
+)
+
+document.querySelectorAll('.btn_academico').forEach((e) => e.addEventListener('click', () => {
+    if (academicoElement) {
+        academicoElement.scrollIntoView({ behavior: 'smooth' })
+    }
+})
+)
+
+document.querySelectorAll('.btn_certificacoes').forEach((e) => e.addEventListener('click', () => {
+    if (certificacoesElement) {
+        certificacoesElement.scrollIntoView({ behavior: 'smooth' })
+    }
+})
+)
+
+document.querySelectorAll('.btn_softskills').forEach((e) => e.addEventListener('click', () => {
+    if (softskillsElement) {
+        softskillsElement.scrollIntoView({ behavior: 'smooth' })
+    }
+})
+)
+
+logoElement.addEventListener('click', () => {
+    headerElement.scrollIntoView({ behavior: 'smooth' })
+})
+
+btnMenuMobileElement.addEventListener('click', function novo(e) {
+    if (apertouMenu) {
+
+        btnMenuMobileElement.style = 'rotate: 90deg'
+
+        asideMenuMobile.style = 'right: 0vw'
+
+        setInterval(() => apertouMenu = false, 100)
+    }
+
+    if (apertouMenu === false) {
+
+        btnMenuMobileElement.style = 'rotate: 0deg'
+
+        asideMenuMobile.style = 'right: -100%'
+
+        setInterval(() => apertouMenu = true, 100)
+    }
+})
+
+newlayout2()
+
+window.addEventListener('resize', newlayout2)
